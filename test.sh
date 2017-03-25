241e587b50543428a5a047c3671340af68e66b6d
magnus <magnus@genesilico.pl>
2017-03-25 13:20:21 +0100

    fix ./ in test.sh

--- test.sh
+++ test.sh
@@ -1,3 +1,3 @@
-scanify.py test_data/calculusone.png
-scanify.py --grayscale test_data/calculusone2.png
-resize.py test_data/calculusone.png
+./scanify.py test_data/calculusone.png
+./scanify.py --grayscale test_data/calculusone2.png
+./resize.py test_data/calculusone.png
